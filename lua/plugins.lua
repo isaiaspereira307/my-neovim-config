5fab13985580c1258083e5e242a43a73c22edff3
Isaias Pereira <58946382+isaiaspereira307@users.noreply.github.com>
2022-08-25 16:03:21 -0300

    Update plugins.lua

--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,22 +1,17 @@
--- This file can be loaded by calling `lua require('plugins')` from your init.vim
-
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
-	use 'preservim/nerdtree'
-	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true } }
-	use "windwp/nvim-autopairs"
-	use 'nvie/vim-flake8'
-	use 'python-mode/python-mode'
-	use 'ryanoasis/vim-devicons'
-	use 'lewis6991/gitsigns.nvim'
-	use 'xuyuanp/nerdtree-git-plugin'
-	use 'bling/vim-bufferline'
-	use "lukas-reineke/indent-blankline.nvim"
-	use 'kyazdani42/nvim-web-devicons'
+	use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
+	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true }}
+	use 'windwp/nvim-autopairs'
+        use 'nvie/vim-flake8'
+        use 'python-mode/python-mode'
+        use 'ryanoasis/vim-devicons'
+        use 'lewis6991/gitsigns.nvim'
+        use 'xuyuanp/nerdtree-git-plugin'
 	use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
-	use 'dstein64/nvim-scrollview'
-	use 'https://github.com/morhetz/gruvbox.git'
-	use 'https://github.com/sainnhe/gruvbox-material.git'
+        use 'kyazdani42/nvim-web-devicons'
+        use 'dstein64/nvim-scrollview'
+        use "EdenEast/nightfox.nvim" -- Packer
 	use {"akinsho/toggleterm.nvim", tag = 'v2.*'}
 	use 'neovim/nvim-lspconfig' -- sudo npm i -g pyright typescript typescript-language-server
 	use 'nvim-lua/plenary.nvim'
