497884ea0f95cc2f46bce4ecdf1fd7e95d36bc6f
isaiaspereira307 <isaias.pereira@protonmail.com>
2024-02-19 21:44:31 -0300

    ajuste de config

--- init.vim
+++ init.vim
@@ -1,46 +1,19 @@
 lua require('plugins')
-lua require('nvim-web-devicons').setup()
-lua require("nvim-tree").setup()
-lua require("lualine").setup() 
 lua require("nvim-autopairs").setup()
+lua require("nvim-tree").setup()
+lua require("lualine").setup()
 lua require('gitsigns').setup()
-lua require("toggleterm").setup()
-lua require("bufferline").setup()
-lua require("nvim-lsp-installer").setup{}
-lua require("lspconfig")['pyright'].setup{ on_attach = on_attach, flags = lsp_flags, }
-lua require("lspconfig")['rust_analyzer'].setup{ on_attach = on_attach, flags = lsp_flags, settings = { ["rust-analyzer"] = {} } }
-lua require("rust-tools").setup()
+lua require('telescope').setup()
+lua require('lsp-zero').setup()
+
 set nu!
 set mouse=a
 set title
 set cursorline
 set encoding=utf-8
 set background=dark
-set foldenable
-set foldmethod=indent
-set clipboard=unnamed
-" zo - open fold
-" zc - close fold
-" zM - close all fold
-" zR - Unfold all
 syntax on
-colorscheme nightfox
-let g:bufferline_echo = 1
-let g:bufferline_modified = '+'
-let g:bufferline_active_buffer_left = '['
-let g:bufferline_active_buffer_right = ']'
-let g:scrollview_excluded_filetypes = ['nvim-tree']
-let g:scrollview_current_only = 1
-let g:scrollview_winblend = 75
-" Position the scrollbar at the 80th character of the buffer
-let g:scrollview_base = 'buffer'
-let g:scrollview_column = 80
-" In your init.lua or init.vim
-set termguicolors
-" comment lines  :15,25s/^/#
-map q :quit<CR>
-map <C-s> :write<CR>
-map <C-w> :tabn<CR>
-map <C-Space> :ToggleTerm<CR>
-nnoremap <C-n> :NvimTreeToggle<CR>
+colorscheme catppuccin
 
+map <C-w> :tabn<CR>
+nnoremap <C-n> :NvimTreeToggle<CR>
--- lua/plugins.lua
+++ lua/plugins.lua
@@ -1,45 +1,28 @@
 return require('packer').startup(function(use)
 	use 'wbthomason/packer.nvim'
+	use 'windwp/nvim-autopairs'
 	use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
 	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true }}
-	use 'windwp/nvim-autopairs'
-        use 'nvie/vim-flake8'
-        use 'python-mode/python-mode'
-        use 'ryanoasis/vim-devicons'
-        use 'lewis6991/gitsigns.nvim'
+	use 'lewis6991/gitsigns.nvim'
         use 'xuyuanp/nerdtree-git-plugin'
-	use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
-        use 'kyazdani42/nvim-web-devicons'
-        use 'dstein64/nvim-scrollview'
-        use "EdenEast/nightfox.nvim" -- Packer
-	use {"akinsho/toggleterm.nvim", tag = 'v2.*'}
-	use 'neovim/nvim-lspconfig' -- sudo npm i -g pyright typescript typescript-language-server
-	use 'nvim-lua/plenary.nvim'
-	use 'nvim-telescope/telescope.nvim'
-	-- use { "nvim-telescope/telescope-file-browser.nvim" }
+	use 'ryanoasis/vim-devicons'
+	use { "catppuccin/nvim", as = "catppuccin" }
+	use("sheerun/vim-polyglot")
+	use {
+ 		 'nvim-telescope/telescope.nvim', tag = '0.1.5',
+  	requires = { {'nvim-lua/plenary.nvim'} }
+	}
+	use("tpope/vim-fugitive")
+	use {
+  	'VonHeikemen/lsp-zero.nvim',
+  	branch = 'v3.x',
+  	requires = {
+    	-- LSP Support
+    	{'neovim/nvim-lspconfig'},
+    	-- Autocompletion
+   	 	{'hrsh7th/nvim-cmp'},
+    	{'hrsh7th/cmp-nvim-lsp'},
+    	{'L3MON4D3/LuaSnip'},
+  	}
+	}
 end)
-
-return require('packer').startup(function(use)
-	use 'wbthomason/packer.nvim'
-	use { 'kyazdani42/nvim-tree.lua', requires = { 'kyazdani42/nvim-web-devicons' } }
-	use { 'nvim-lualine/lualine.nvim', requires = { 'kyazdani42/nvim-web-devicons', opt = true }}
-	use 'windwp/nvim-autopairs'
-        use 'nvie/vim-flake8'
-        use 'ryanoasis/vim-devicons'
-	use {'neoclide/coc.nvim', branch = 'release'}
-        use 'lewis6991/gitsigns.nvim'
-        use 'xuyuanp/nerdtree-git-plugin'
-	use {'akinsho/bufferline.nvim', tag = "v2.*", requires = 'kyazdani42/nvim-web-devicons'}
-        use 'kyazdani42/nvim-web-devicons'
-        use 'dstein64/nvim-scrollview'
-        use "EdenEast/nightfox.nvim" -- Packer
-	use {"akinsho/toggleterm.nvim", tag = 'v2.*'}
-	use 'neovim/nvim-lspconfig'
-	use "williamboman/nvim-lsp-installer"
-	use 'simrat39/rust-tools.nvim'
-	use {"ray-x/lsp_signature.nvim"}
-	use 'L3MON4D3/LuaSnip'
-	use 'onsails/lspkind.nvim'
-end)
-
-
